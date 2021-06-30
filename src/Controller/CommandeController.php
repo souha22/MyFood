d5990e90c25f96ed0944bb29f4b15602e1f5d8c1
Ahlem17-03 <66637401+Ahlem17-03@users.noreply.github.com>
2021-06-30 01:00:51 +0100

    commit commande

---
+++ src/Controller/CommandeController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class CommandeController extends AbstractController
+{
+    /**
+     * @Route("/commande", name="commande")
+     */
+    public function index(): Response
+    {
+        return $this->json([
+            'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/CommandeController.php',
+        ]);
+    }
+}
