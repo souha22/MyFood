342502346cded234f11d8e41c85ef32e5035eeff
Ahlem17-03 <66637401+Ahlem17-03@users.noreply.github.com>
2021-07-21 14:58:19 +0100

    ahlem 21/07

--- src/Controller/CommandeController.php
+++ src/Controller/CommandeController.php
@@ -2,20 +2,70 @@
 
 namespace App\Controller;
 
+use App\Entity\Commande;
+use App\Repository\CommandeRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+
+/**
+ * @Route("/commande", name="commande")
+ */
 class CommandeController extends AbstractController
 {
+    
     /**
-     * @Route("/commande", name="commande")
+     * @var RestServiceController
      */
-    public function index(): Response
+    private $restService;
+
+    public function __construct(RestServiceController  $restService)
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/CommandeController.php',
-        ]);
+        $this->restService = $restService;
+    }
+
+
+    /**
+     * @Route("/getAll", name="Commande_getAll", methods={"GET"})
+     * @param CommandeRepository $repository
+     * @return Response
+     */
+    public function getAllAction(CommandeRepository $repository): Response
+    {
+        return $this->restService->getAllAction($repository);
+    }
+
+    /**
+     * @Route("/add", name="Commande_create", methods={"POST"})
+     * @param Request $request
+     * @return Response
+     */
+    public function createAction(Request $request): Response
+    {
+        return $this->restService->createAction($request, "App\Entity\Commande");
+    }
+
+    /**
+     * @Route("/update/{id}", name="Commande_update", methods={"PUT"})
+     * @return Response
+     */
+
+    public function updateAction(Commande $Commande, Request $request): Response
+    {
+        return $this->restService->updateAction($Commande, $request);
+    }
+
+    /**
+     * @Route("/delete/{id}", name="Commande_delete", methods={"DELETE"})
+     * @param int $id
+     * @param CommandeRepository $repository
+     * @return Response
+     */
+
+    public function deleteAction($id, CommandeRepository $repository): Response
+    {
+        return $this->restService->deleteAction($id, $repository);
     }
 }
